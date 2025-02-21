60a3e4ac790a58e469753638787f0105643585df
TumLe612 <lehoangquan10m@gmail.com>
2025-02-21 10:19:12 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 {
   let u=us.value
   let p=pw.value
-  if (u==quan" && p=="123")
+  if (u=="quan" && p=="123")
   {
     window.open("home.html")
     document.getElementById(compiler).style.display="block"
