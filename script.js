7efdcbfe09ba9bc74f481f6c7ef6c65759105802
TumLe612 <lehoangquan10m@gmail.com>
2025-02-21 10:22:09 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function login()
 {
   let u=us.value
   let p=pw.value
-  if (u=="quan" && p=="123")
+  if (u=="Quan" && p=="123")
   {
     window.open("Home.html")
     document.getElementById(compiler).style.display="block"
